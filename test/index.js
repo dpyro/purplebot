ef2931dcc47b8a2bd25a9dd6c207d01656675717
Sumant Manne <sumant.manne@gmail.com>
2017-04-17 18:17:29 -0500

    Added invoke test

---
+++ test/index.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect
+const exec = require('child_process').exec
+
+describe('index', function () {
+  it('npm start can print --help', function () {
+    this.slow(2000)
+
+    return new Promise(function (resolve) {
+      exec('npm start -- --help', (error, stdout, stderr) => {
+        expect(error).to.not.exist
+        expect(stdout + stderr).to.have.a.string('help')
+        resolve()
+      })
+    })
+  })
+})
