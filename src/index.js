f613ac0782f9902283e9ace124aef3a22dbb536d
Sumant Manne <sumant.manne@gmail.com>
2017-04-17 17:41:09 -0500

    Added yargs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
+#!/usr/bin/env node
 
 const PurpleBot = require('./bot.js')
 
-const options = {
-  server: 'localhost',
-  channels: ['#test']
+if (require.main === module) {
+  const yargs = require('yargs')
+    .usage('Usage: $0 [-s server.address:port] [-c #channel1 #channel2 ...]')
+    .options('c', {
+      alias: 'channels',
+      default: [],
+      describe: 'channels to join upon connect',
+      type: 'array'
+    })
+    .option('s', {
+      alias: 'server',
+      default: 'localhost',
+      describe: 'connect to this server',
+      nargs: 1,
+      type: 'string'
+    })
+    .option('v', {
+      alias: 'verbose',
+      describe: 'Print debugging output',
+      type: 'boolean'
+    })
+    .help()
+  const argv = yargs.argv
+
+  if (argv.help) {
+    yargs.showHelp()
+    process.exit(0)
+  }
+
+  const bot = new PurpleBot({
+    server: argv.server,
+    channels: argv.channels,
+    debug: argv.v
+  })
+  bot.enable()
 }
-const bot = new PurpleBot(options)
-bot.enable()
