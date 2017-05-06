823dfa7218c04e5e4219d81005645bb4da6c9734
Sumant Manne <sumant.manne@gmail.com>
2017-05-06 16:35:35 -0500

    Moves logger var

--- plugins/logging.js
+++ plugins/logging.js
@@ -39,100 +39,6 @@ function timestamp () {
 }
 
 /**
- * Logger functions.
- *
- * @type {Object<string, function(...any): string>}
- *
- * @private
- */
-const loggers = {
-  'connect': (server) => {
-    return `CONNECT ${server}`
-  },
-  'disconnect': (server, message) => {
-    const msg = (message) ? `: ${message}` : ''
-    return `DISCONNECT ${server}${msg}`
-  },
-  'action': (from, to, text, message) => {
-    return `ACTION ${from} → ${to}: ${text}`
-  },
-  'ctcp-notice': (from, to, text, message) => {
-    return `CTCP NOTICE ${from} → ${to}: ${text}`
-  },
-  'ctcp-privmsg': (from, to, text, message) => {
-    return `CTCP PRIVMSG ${from} → ${to}: ${text}`
-  },
-  'ctcp-version': (from, to, message) => {
-    return `CTCP VERSION ${from} → ${to}`
-  },
-  'invite': (channel, from, message) => {
-    return `INVITE ${from} → ${channel}`
-  },
-  'join': (channel, who, message) => {
-    return `JOIN ${who} → ${channel}`
-  },
-  'kick': (channel, who, by, reason) => {
-    return `KICK ${by} → ${channel}: ${who} ${reason}`
-  },
-  'kill': (nick, reason, channels, message) => {
-    return `KILL ${nick} → ${channels.join(' ')}: ${reason}`
-  },
-  '+mode': (channel, by, mode, argument, message) => {
-    return [`MODE ${by} → +${mode} ${channel}`, argument].filter(e => e).join(' ')
-  },
-  '-mode': (channel, by, mode, argument, message) => {
-    return [`MODE ${by} → -${mode} ${channel}`, argument].filter(e => e).join(' ')
-  },
-  'motd': (motd) => {
-    return `MOTD ${motd}`
-  },
-  'msg': (nick, to, text, message) => {
-    return `${nick} → ${to}: ${text}`
-  },
-  'names': (channel, names) => {
-    const nameString = _.toPairs(names).map(([name, mode]) => {
-      return `${mode}${name}`
-    }).join(' ')
-    return `NAMES ${channel}: ${nameString}`
-  },
-  'nick': (oldnick, newnick, channels, message) => {
-    return `NICK ${oldnick} → ${newnick} on ${channels.join(' ')}`
-  },
-  'notice': (nick, to, text, message) => {
-    const nickString = (nick != null) ? `${nick} → ` : ''
-    return `NOTICE ${nickString}${to}: ${text}`
-  },
-  'part': (channel, who, reason) => {
-    return `PART ${channel} → ${who}: ${reason}`
-  },
-  'pm': (nick, text, message) => {
-    return `PM ${nick}: ${text}`
-  },
-  'quit': (nick, reason, channels, message) => {
-    return `QUIT ${nick}: ${reason}`
-  },
-  'registered': (message) => {
-    return `REGISTERED`
-  },
-  'selfMessage': (to, text) => {
-    return `${to} ← ${text}`
-  },
-  'topic': (channel, topic, nick, message) => {
-    return `TOPIC ${nick} → ${channel}: ${topic}`
-  },
-  'whois': (info) => {
-    const output = _.toPairs(info).map(([key, value]) => {
-      if (_.isArray(value)) {
-        value = value.join(' ')
-      }
-      return `\t${key}: ${value}`
-    }).join('\n')
-
-    return `WHOIS\n${output}`
-  }
-}
-
-/**
  * Plugin for logging sent and recieved messages.
  *
  * @implements {module:purplebot.Plugin}
@@ -140,6 +46,100 @@ const loggers = {
  * @memberof module:purplebot
  */
 class LoggingPlugin {
+    /**
+   * Logger functions.
+   *
+   * @type {Object<string, function(...any): string>}
+   *
+   * @private
+   */
+  static loggers = {
+    'connect': (server) => {
+      return `CONNECT ${server}`
+    },
+    'disconnect': (server, message) => {
+      const msg = (message) ? `: ${message}` : ''
+      return `DISCONNECT ${server}${msg}`
+    },
+    'action': (from, to, text, message) => {
+      return `ACTION ${from} → ${to}: ${text}`
+    },
+    'ctcp-notice': (from, to, text, message) => {
+      return `CTCP NOTICE ${from} → ${to}: ${text}`
+    },
+    'ctcp-privmsg': (from, to, text, message) => {
+      return `CTCP PRIVMSG ${from} → ${to}: ${text}`
+    },
+    'ctcp-version': (from, to, message) => {
+      return `CTCP VERSION ${from} → ${to}`
+    },
+    'invite': (channel, from, message) => {
+      return `INVITE ${from} → ${channel}`
+    },
+    'join': (channel, who, message) => {
+      return `JOIN ${who} → ${channel}`
+    },
+    'kick': (channel, who, by, reason) => {
+      return `KICK ${by} → ${channel}: ${who} ${reason}`
+    },
+    'kill': (nick, reason, channels, message) => {
+      return `KILL ${nick} → ${channels.join(' ')}: ${reason}`
+    },
+    '+mode': (channel, by, mode, argument, message) => {
+      return [`MODE ${by} → +${mode} ${channel}`, argument].filter(e => e).join(' ')
+    },
+    '-mode': (channel, by, mode, argument, message) => {
+      return [`MODE ${by} → -${mode} ${channel}`, argument].filter(e => e).join(' ')
+    },
+    'motd': (motd) => {
+      return `MOTD ${motd}`
+    },
+    'msg': (nick, to, text, message) => {
+      return `${nick} → ${to}: ${text}`
+    },
+    'names': (channel, names) => {
+      const nameString = _.toPairs(names).map(([name, mode]) => {
+        return `${mode}${name}`
+      }).join(' ')
+      return `NAMES ${channel}: ${nameString}`
+    },
+    'nick': (oldnick, newnick, channels, message) => {
+      return `NICK ${oldnick} → ${newnick} on ${channels.join(' ')}`
+    },
+    'notice': (nick, to, text, message) => {
+      const nickString = (nick != null) ? `${nick} → ` : ''
+      return `NOTICE ${nickString}${to}: ${text}`
+    },
+    'part': (channel, who, reason) => {
+      return `PART ${channel} → ${who}: ${reason}`
+    },
+    'pm': (nick, text, message) => {
+      return `PM ${nick}: ${text}`
+    },
+    'quit': (nick, reason, channels, message) => {
+      return `QUIT ${nick}: ${reason}`
+    },
+    'registered': (message) => {
+      return `REGISTERED`
+    },
+    'selfMessage': (to, text) => {
+      return `${to} ← ${text}`
+    },
+    'topic': (channel, topic, nick, message) => {
+      return `TOPIC ${nick} → ${channel}: ${topic}`
+    },
+    'whois': (info) => {
+      const output = _.toPairs(info).map(([key, value]) => {
+        if (_.isArray(value)) {
+          value = value.join(' ')
+        }
+        return `\t${key}: ${value}`
+      }).join('\n')
+
+      return `WHOIS\n${output}`
+    }
+  }
+
   /**
    * Creates an attached instance of `LoggingPlugin`.
    *
@@ -161,8 +161,8 @@ class LoggingPlugin {
       stream = fs.createWriteStream(filePath, { flags: 'a' })
     }
 
-    for (let eventName of Object.keys(loggers)) {
-      const callback = loggers[eventName]
+    for (let eventName of Object.keys(LoggingPlugin.loggers)) {
+      const callback = LoggingPlugin.loggers[eventName]
       onSafe(bot, eventName, (...args) => {
         const data = callback.apply(null, args)
         const line = `[${timestamp()}] ${data}\n`
