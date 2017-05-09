8a90242f406978024d83abd306c71593cfcc254d
Sumant Manne <sumant.manne@gmail.com>
2017-05-09 01:55:08 -0500

    Reformat LoggingPlugin.loggers

--- plugins/logging.ts
+++ plugins/logging.ts
@@ -40,80 +40,83 @@ export default class LoggingPlugin implements Plugin {
    * Logger functions.
    */
   protected static loggers = {
-    'connect': (server) => {
-      return `CONNECT ${server}`
-    },
+    'connect': (server) =>
+      `CONNECT ${server}`,
+
     'disconnect': (server, message) => {
       const msg = (message) ? `: ${message}` : ''
       return `DISCONNECT ${server}${msg}`
     },
-    'action': (from, to, text, message) => {
-      return `ACTION ${from} → ${to}: ${text}`
-    },
-    'ctcp-notice': (from, to, text, message) => {
-      return `CTCP NOTICE ${from} → ${to}: ${text}`
-    },
-    'ctcp-privmsg': (from, to, text, message) => {
-      return `CTCP PRIVMSG ${from} → ${to}: ${text}`
-    },
-    'ctcp-version': (from, to, message) => {
-      return `CTCP VERSION ${from} → ${to}`
-    },
-    'invite': (channel, from, message) => {
-      return `INVITE ${from} → ${channel}`
-    },
-    'join': (channel, who, message) => {
-      return `JOIN ${who} → ${channel}`
-    },
-    'kick': (channel, who, by, reason) => {
-      return `KICK ${by} → ${channel}: ${who} ${reason}`
-    },
-    'kill': (nick, reason, channels, message) => {
-      return `KILL ${nick} → ${channels.join(' ')}: ${reason}`
-    },
-    '+mode': (channel, by, mode, argument, message) => {
-      return [`MODE ${by} → +${mode} ${channel}`, argument].filter(e => e).join(' ')
-    },
-    '-mode': (channel, by, mode, argument, message) => {
-      return [`MODE ${by} → -${mode} ${channel}`, argument].filter(e => e).join(' ')
-    },
-    'motd': (motd) => {
-      return `MOTD ${motd}`
-    },
-    'msg': (nick, to, text, message) => {
-      return `${nick} → ${to}: ${text}`
-    },
+
+    'action': (from, to, text, message) =>
+      `ACTION ${from} → ${to}: ${text}`,
+
+    'ctcp-notice': (from, to, text, message) =>
+      `CTCP NOTICE ${from} → ${to}: ${text}`,
+
+    'ctcp-privmsg': (from, to, text, message) =>
+      `CTCP PRIVMSG ${from} → ${to}: ${text}`,
+
+    'ctcp-version': (from, to, message) =>
+      `CTCP VERSION ${from} → ${to}`,
+
+    'invite': (channel, from, message) =>
+      `INVITE ${from} → ${channel}`,
+
+    'join': (channel, who, message) =>
+      `JOIN ${who} → ${channel}`,
+
+    'kick': (channel, who, by, reason) =>
+      `KICK ${by} → ${channel}: ${who} ${reason}`,
+
+    'kill': (nick, reason, channels, message) =>
+      `KILL ${nick} → ${channels.join(' ')}: ${reason}`,
+
+    '+mode': (channel, by, mode, argument, message) =>
+      _.compact([`MODE ${by} → +${mode} ${channel}`, argument]).join(' '),
+
+    '-mode': (channel, by, mode, argument, message) =>
+      _.compact([`MODE ${by} → -${mode} ${channel}`, argument]).join(' '),
+
+    'motd': (motd) =>
+      `MOTD ${motd}`,
+
+    'msg': (nick, to, text, message) =>
+      `${nick} → ${to}: ${text}`,
+
     'names': (channel, names) => {
       const nameString = _.toPairs(names).map(([name, mode]) => {
         return `${mode}${name}`
       }).join(' ')
       return `NAMES ${channel}: ${nameString}`
     },
-    'nick': (oldnick, newnick, channels, message) => {
-      return `NICK ${oldnick} → ${newnick} on ${channels.join(' ')}`
-    },
+
+    'nick': (oldnick, newnick, channels, message) =>
+      `NICK ${oldnick} → ${newnick} on ${channels.join(' ')}`,
+
     'notice': (nick, to, text, message) => {
       const nickString = (nick != null) ? `${nick} → ` : ''
       return `NOTICE ${nickString}${to}: ${text}`
     },
-    'part': (channel, who, reason) => {
-      return `PART ${channel} → ${who}: ${reason}`
-    },
-    'pm': (nick, text, message) => {
-      return `PM ${nick}: ${text}`
-    },
-    'quit': (nick, reason, channels, message) => {
-      return `QUIT ${nick}: ${reason}`
-    },
-    'registered': (message) => {
-      return `REGISTERED`
-    },
-    'selfMessage': (to, text) => {
-      return `${to} ← ${text}`
-    },
-    'topic': (channel, topic, nick, message) => {
-      return `TOPIC ${nick} → ${channel}: ${topic}`
-    },
+
+    'part': (channel, who, reason) =>
+      `PART ${channel} → ${who}: ${reason}`,
+
+    'pm': (nick, text, message) =>
+      `PM ${nick}: ${text}`,
+
+    'quit': (nick, reason, channels, message) =>
+      `QUIT ${nick}: ${reason}`,
+
+    'registered': (message) =>
+      `REGISTERED`,
+
+    'selfMessage': (to, text) =>
+      `${to} ← ${text}`,
+
+    'topic': (channel, topic, nick, message) =>
+      `TOPIC ${nick} → ${channel}: ${topic}`,
+
     'whois': (info) => {
       const output = _.toPairs(info).map(([key, value]) => {
         if (_.isArray(value)) {
