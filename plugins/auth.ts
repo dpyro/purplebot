fbcaee45d072d37c1463090c38a7fb6334ce8aa9
Sumant Manne <sumant.manne@gmail.com>
2017-05-12 11:14:16 -0500

    Add auth plugin

---
+++ plugins/auth.ts
@@ -0,0 +1,37 @@
+/**
+ * @author Sumant Manne <sumant.manne@gmail.com>
+ * @license MIT
+ */
+
+import { Plugin } from '../src/plugins'
+import PurpleBot from '../src/bot'
+import Config from '../src/config'
+
+/**
+ * Plugin to authenticate with NickServ
+ */
+export default class AuthPlugin implements Plugin {
+  readonly name = 'auth'
+
+  bot: PurpleBot
+  config: Config
+
+  async load (bot: PurpleBot, config: Config): Promise<void> {
+    this.bot = bot
+    this.config = config
+
+    bot.on('connect', async (server) => {
+      const nickname = await config.get(`${this.name}:nickname`)
+      if (nickname == null) return false
+      const password = await config.get(`${this.name}:password`)
+
+      if (password != null) {
+        bot.say('NickServ', `GHOST ${nickname} ${password}`)
+      }
+      bot.setNick(nickname)
+      if (password != null) {
+        bot.say('NickServ', `IDENTIFY ${password}`)
+      }
+    })
+  }
+}
